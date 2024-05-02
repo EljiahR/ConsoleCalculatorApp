7e559cf082bc2643e40542a22de4a42c7def3ca8
Elijah Reck <kcerhajile@gmail.com>
2024-05-01 21:54:48 -0400

    Added methods for collecting user input and evaluating expressions

--- ConsoleCalculator/Program.cs
+++ ConsoleCalculator/Program.cs
@@ -2,13 +2,44 @@
 Console.WriteLine("----------------------");
 Console.WriteLine();
 
-Console.WriteLine(Calc.Add(1, 5));
+Calc.AskOperator();
+string? operatorInput = Console.ReadLine();
+while(operatorInput != "a" && operatorInput != "s" && operatorInput != "m" && operatorInput != "d")
+{
+    Console.WriteLine("Invalid choice.");
+    Calc.AskOperator();
+    operatorInput = Console.ReadLine();
+}
 
+double num1 = Calc.AskNumber();
+double num2 = Calc.AskNumber();
+double result = Calc.Evaluate(operatorInput, num1, num2);
+Console.WriteLine(result);
 
 
 class Calc
 {
-      
+    public static void AskOperator()
+    {
+        Console.WriteLine("Please select from the following");
+        Console.WriteLine("\ta - Addition");
+        Console.WriteLine("\ts - Subtraction");
+        Console.WriteLine("\tm - Multiplication");
+        Console.WriteLine("\td - Division");
+    }
+
+    public static double AskNumber()
+    {
+        Console.WriteLine("Please enter a number:");
+        string? input = Console.ReadLine();
+        double num;
+        while (!Double.TryParse(input, out num))
+        {
+            Console.WriteLine("Please enter a VALID number:");
+            input = Console.ReadLine();
+        }
+        return num;
+    }
     public static double Add(double x, double y) => x + y;
     public static double Subtract(double x, double y) => x - y;
     public static double Multiply(double x, double y) => x * y;
@@ -22,6 +53,24 @@ class Calc
         {
             Console.WriteLine(ex.Message);
             return 0;
+        }
+    }
+
+    public static double Evaluate(string operatorInput, double x, double y)
+    {
+        switch(operatorInput)
+        {
+            case "a":
+                return Add(x, y);
+            case "s":
+                return Subtract(x, y);
+            case "m":
+                return Multiply(x, y);
+            case "d":
+                return Divide(x, y);
+            default:
+                Console.WriteLine("Error occured during evaluation");
+                return 0;
         }
     }
 }
