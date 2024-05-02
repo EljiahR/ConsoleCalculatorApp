bd9aa790c21a7b0944c594e23971e602a2457348
Elijah Reck <kcerhajile@gmail.com>
2024-05-01 21:34:49 -0400

    Added Calc class for containing operator functions

--- ConsoleCalculator/Program.cs
+++ ConsoleCalculator/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Console Calculator App");
+Console.WriteLine("----------------------");
+Console.WriteLine();
+
+Console.WriteLine(Calc.Add(1, 5));
+
+
+
+class Calc
+{
+      
+    public static double Add(double x, double y) => x + y;
+    public static double Subtract(double x, double y) => x - y;
+    public static double Multiply(double x, double y) => x * y;
+    public static double Divide(double x, double y)
+    {
+        try
+        {
+            return x / y;
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine(ex.Message);
+            return 0;
+        }
+    }
+}
+
+
